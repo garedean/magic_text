334f9efca3b8622946099ccb61ec5df0a8eb447d
James Williams & Garrett Olson <student@epicodus.com>
2015-07-20 12:25:36 -0700

    add contacts to user page, create form for adding contact, create relationship in DB update schema

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,32 @@
+class ContactsController < ApplicationController
+
+  def index
+    @contacts = current_user.contacts
+  end
+
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = current_user.contacts.new(contact_params)
+    if @contact.save
+      redirect_to user_contacts_path(current_user), notice: 'Successfully added contact'
+    else
+      render :new, alert: 'Issue adding contact'
+    end
+  end
+
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :phone)
+  end
+
+end
